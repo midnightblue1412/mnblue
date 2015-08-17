67086567771796a1e8974a4031f7096ee387fe0c
midnightblue1412 <solar_flare252@yahoo.com.ph>
2015-08-17 17:31:59 +0800

    added binarynode class

---
+++ src/src/datastructures/BinaryNode.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.datastructures;
+
+/**
+ *
+ * @author De la Cruz Family
+ * @param <K>   Key
+ * @param <V>   Value
+ */
+public class BinaryNode<K, V> {
+    protected K _key;
+    protected V _value;
+    
+    protected BinaryNode<K, V> parent;
+    protected BinaryNode<K, V> leftChild;
+    protected BinaryNode<K, V> rightChild;
+    
+    public BinaryNode(K key, V value)
+    {
+        _key = key;
+        _value = value;
+    }
+    
+    public K key()
+    {
+        return _key;
+    }
+    
+    public V value()
+    {
+        return _value;
+    }
+    
+    public void setValue(V value)
+    {
+        _value = value;
+    }
+}
